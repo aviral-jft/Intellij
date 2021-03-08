5c330b53617b6b78419e85b580e0dd88638cd2b0
Aviral Gupta <aviral.gupta@jellyfishtechnologies.com>
2021-03-08 11:51:58 +0530

    MVC-4

---
+++ MVC-4/src/main/java/com/spring/HelloController.java
@@ -0,0 +1,19 @@
+package com.spring;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+        @RequestMapping("/hello")
+        public String redrirect()
+        {
+            return "viewpage";
+        }
+        @RequestMapping("/helloagain")
+    public String display()
+        {
+            return "final";
+        }
+    }
+
+
